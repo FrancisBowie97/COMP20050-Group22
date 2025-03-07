d570d5ed76375d07015ce5ded38f6770b5b56272
Harsh Palwal <hpaliwal99@gmail.com>
2025-03-07 10:58:08 +0000

    Added the coordinate system and conversion functions. Added Hex_db to store the Nodes.

--- src/main/java/com/hex/hex/boardController.java
+++ src/main/java/com/hex/hex/boardController.java
@@ -4,12 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-/**
- * Sample Skeleton for 'hello-view.fxml' Controller Class
- */
+
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import javafx.scene.input.MouseEvent;
@@ -18,9 +17,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 
-import static javafx.scene.paint.Color.color;
+import static java.lang.Integer.parseInt;
 
-enum Player {RED, BLUE}; // enum to keep track of player turns
+enum Player {RED, BLUE} // enum to keep track of player turns
 
 /**
  * Board Class to control and store data regarding Cells and Nodes.
@@ -28,8 +27,6 @@ enum Player {RED, BLUE}; // enum to keep track of player turns
 
 public class boardController {
     Player Turn = Player.RED;
-//    int user1 = 0;
-//    int user2 = 0;
 
 
     @FXML // ResourceBundle that was given to the FXMLLoader
@@ -50,6 +47,8 @@ public class boardController {
     private ArrayList<Circle> circles = new ArrayList<>();
     private Circle TempCircle;
 
+    private Node[][] Hex_db = new Node[13][13];
+
     /**
      * Function to handle clinking on a Cell.
      * @param mouseEvent pass the mouse event to function.
@@ -57,55 +56,123 @@ public class boardController {
     @FXML
     public void getHexID(javafx.scene.input.MouseEvent mouseEvent) {
 
-        if(Turn == Player.RED){
-            Polygon hexagon = (Polygon) mouseEvent.getSource();
-            double x = hexagon.getLayoutX();
-            double y = hexagon.getLayoutY();
+        Polygon hexagon = (Polygon) mouseEvent.getSource();
+        double x = hexagon.getLayoutX();
+        double y = hexagon.getLayoutY();
 
-            Circle circle = new Circle(x, y, 32.5);
-            circles.add(circle);
-            circle.setFill(Color.RED);
-            ((AnchorPane) hexagon.getParent()).getChildren().add(circle);
-            Turn = Player.BLUE;
-//            user1 = 1;
-//            user2 = 0;
-            AnchorPane parent = (AnchorPane) hexagon.getParent();
-            displayTurn(parent);
-        }
-        else if(Turn == Player.BLUE){
-            Polygon hexagon = (Polygon) mouseEvent.getSource();
-            double x = hexagon.getLayoutX();
-            double y = hexagon.getLayoutY();
+        System.out.println(x + " " + y + " " + Turn);
 
-            Circle circle = new Circle(x, y, 32.5);
-            circles.add(circle);
-            circle.setFill(Color.BLUE);
-            ((AnchorPane) hexagon.getParent()).getChildren().add(circle);
-            Turn = Player.RED;
-//            user2 = 1;
-//            user1 = 0;
-            AnchorPane parent = (AnchorPane) hexagon.getParent();
-            displayTurn(parent);
+
+        AnchorPane parent = (AnchorPane) hexagon.getParent();
+
+        if (isValid(hexagon)) {
+
+            // Storing location info of the Cell.
+            Node temp = new Node();
+            String[] cord = Cord_convert(hexagon);
+            int[] cord_index = Cord_to_index(cord);
+
+            if(Turn == Player.RED){
+
+                Circle circle = new Circle(x, y, 32.5);
+                circle.setFill(Color.RED);
+                circle.setMouseTransparent(true);
+                circles.add(circle);
+                Hex_db[cord_index[0]][cord_index[1]] = temp;
+                parent.getChildren().add(circle);
+                Turn = Player.BLUE;
+
+                displayTurn(parent);
+            }
+            else if(Turn == Player.BLUE){
+
+                Circle circle = new Circle(x, y, 32.5);
+                circle.setFill(Color.BLUE);
+                circle.setMouseTransparent(true);
+                circles.add(circle);
+                Hex_db[cord_index[0]][cord_index[1]] = temp;
+                parent.getChildren().add(circle);
+                Turn = Player.RED;
+
+                displayTurn(parent);
+            }
         }
+
+
+        //Temp Debugging
+        Label Dis_cord;
+        Dis_cord = new Label(Arrays.toString(hexagon.getId().split("_")));
+        Dis_cord.setLayoutX(x-5);
+        Dis_cord.setLayoutY(y-5);
+        Dis_cord.setMouseTransparent(true);
+        parent.getChildren().add(Dis_cord);
 
     }
+
+    /**
+     * Function to convert alphanumeric coordinates to array index for ease of access.
+     * @param cord alphanumeric coordinates from Cord_convert().
+     * @return array index to access Cell info.
+     */
+    private int[] Cord_to_index(String[] cord) {
+        int[] cord_ind = new int[2];
+        cord_ind[0] = (cord[0].toCharArray()[0] - 65);
+        cord_ind[1] = (parseInt(cord[1]) - 1);
+        return cord_ind;
+    }
+
+    /**
+     * Function to determine if a move is valid not. (currently only checks if cell is empty).
+     * @param hexagon pass the concerned cell.
+     * @return Boolean value depending on validity of move.
+     */
+    private boolean isValid(Polygon hexagon) {
+        int[] loc = Cord_to_index(Cord_convert(hexagon));
+        if (Hex_db[loc[0]][loc[1]] == null) {
+            System.out.println("Empty");
+            return true;
+        }
+        else {
+            System.out.println("Occupied");
+            return false;
+        }
+    }
+
+    /**
+     * Function to convert X and Y location values into grid coordinate system (e.g. "A1").
+     *
+     * @param object  the object passed into the function
+     * @return Converted grid coordinates
+     */
+    private String[] Cord_convert(Polygon object) {
+        // X starts from 300 up to 972 with 56 step.
+        // Y starts from 41 up to 833 with 33 step.
+        String[] loc;
+        loc = object.getId().split("_");
+        System.out.println(loc[0] + " " + loc[1]);
+
+//        for (char ch : object.getId().toCharArray()) {}
+//        loc[0] += (char) ((x-300)/56);
+//
+//        loc[1] += (char) ((y-41)/33);
+//
+//        System.out.println(loc[0] + ": " + (x-300)/56 + "\n" + loc[1] + ": " + (y-41)/33);
+        return loc;
+    }
+
     private Label turnLabel;
     public void displayTurn(AnchorPane parent){
-        if(turnLabel != null){
-            parent.getChildren().remove(turnLabel);
-        }
-        if(Turn == Player.RED){
+        if(turnLabel == null){
             turnLabel = new Label("Red players turn");
             turnLabel.setLayoutX(1095);
             turnLabel.setLayoutY(375);
             parent.getChildren().add(turnLabel);
-
-        }
-        else if(Turn == Player.BLUE){
-            turnLabel = new Label("Blue players turn");
-            turnLabel.setLayoutX(1095);
-            turnLabel.setLayoutY(375);
-            parent.getChildren().add(turnLabel);
+        } else {
+            if (Turn == Player.RED) {
+                turnLabel.setText("Red players turn");
+            } else if (Turn == Player.BLUE) {
+                turnLabel.setText("Blue players turn");
+            }
         }
     }
 
@@ -146,9 +213,9 @@ public class boardController {
 
     /**
      * Function to handle clicking on the "Close" button to exit the application.
-     * @param actionEvent pass the Action event to the function.
+     *
      */
-    public void Close(ActionEvent actionEvent) {
+    public void Close() {
         System.exit(0);
     }
 
@@ -158,28 +225,30 @@ public class boardController {
      */
     public void highlight(MouseEvent mouseEvent) {
         Polygon hexagon = (Polygon) mouseEvent.getSource();
-        if (TempCircle == null){
-            double x = hexagon.getLayoutX();
-            double y = hexagon.getLayoutY();
+        if (isValid(hexagon)) {
+            if (TempCircle == null){
+                double x = hexagon.getLayoutX();
+                double y = hexagon.getLayoutY();
 
-            Circle circle = new Circle(x, y, 27);
-            //circles.add(circle);
-            circle.setOpacity(0.5);
-            circle.setMouseTransparent(true);  // This allows interaction with the Cell through the TempCircle.
-            ((AnchorPane) hexagon.getParent()).getChildren().add(circle);
-            if (Turn == Player.RED){
-                circle.setFill(Color.RED);
-            }
-            else if(Turn == Player.BLUE){
-                circle.setFill(Color.BLUE);
-            }
-            else {
-                circle.setFill(Color.WHITE);
-            }
-            TempCircle = circle;
-            AnchorPane parent = (AnchorPane) hexagon.getParent();
-            if (!parent.getChildren().contains(circle)) {
-                parent.getChildren().add(circle);
+                Circle circle = new Circle(x, y, 27);
+                //circles.add(circle);
+                circle.setOpacity(0.5);
+                circle.setMouseTransparent(true);  // This allows interaction with the Cell through the TempCircle.
+                ((AnchorPane) hexagon.getParent()).getChildren().add(circle);
+                if (Turn == Player.RED){
+                    circle.setFill(Color.RED);
+                }
+                else if(Turn == Player.BLUE){
+                    circle.setFill(Color.BLUE);
+                }
+                else {
+                    circle.setFill(Color.WHITE);
+                }
+                TempCircle = circle;
+                AnchorPane parent = (AnchorPane) hexagon.getParent();
+                if (!parent.getChildren().contains(circle)) {
+                    parent.getChildren().add(circle);
+                }
             }
         }
 
